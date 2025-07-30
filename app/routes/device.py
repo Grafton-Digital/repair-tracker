38902e2f390a2bcd9fb75498bef68ef60c971ce0
Seán Kelly <sean@override.online>
2025-07-30 19:08:23 +0100

    Add device view route to routes/device.py; Update create_device to use FormData and refresh overview on create; Add delete_device endpoint with existing repair check and refresh on delete; Remove leftover HTMX row update code in table in school_overview.html; Update nav to load new device overview; Create device_overview.html based on school_overview.html with Create Device form as built-in

--- app/routes/device.py
+++ app/routes/device.py
@@ -1,38 +1,77 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, Form, HTTPException, Request, status
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlmodel import select
 from app.models.device import Device, DeviceBase, DevicePublic, DevicesPublic
+from app.models.repair import Repair
 from app.utils.dependencies import session_dep
 import uuid
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
-@router.post("/", response_model=DevicePublic)
-def create_device(*, session: session_dep, device: DeviceBase):
+#
+# Device view routes and HTMX Partials
+#
+
+@router.get("/overview", response_class=HTMLResponse)
+def device_overview(*, session: session_dep, request: Request):
+    devices = session.exec(select(Device)).all()
+    return templates.TemplateResponse(
+        "components/device_overview.html",
+        {"request": request, "devices": devices}
+    )
+
+#
+# Device CRUD routes
+#
+
+@router.post("/", response_class=HTMLResponse)
+def create_device(*, session: session_dep, device: Annotated[DeviceBase, Form()]):
     db_device = Device.model_validate(device)
     session.add(db_device)
     session.commit()
     session.refresh(db_device)
-    return db_device
+    return HTMLResponse(
+        status_code=status.HTTP_201_CREATED,
+        headers={
+            "HX-Trigger": "refreshOverview"
+        }
+    )
 
-@router.get("/", response_model=DevicesPublic)
-def list_devices(*, session: session_dep):
-    devices = session.exec(select(Device)).all()
-    return DevicesPublic(data=devices, count=len(devices))
-
-@router.get("/{device_id}", response_model=DevicePublic)
-def get_device(*, session: session_dep, device_id: uuid.UUID):
-    device = session.get(Device, device_id)
-    if not device:
-        raise HTTPException(status_code=404, detail="Device not found")
-    return device
+# Not in use yet
+# @router.get("/{device_id}", response_model=DevicePublic)
+# def get_device(*, session: session_dep, device_id: uuid.UUID):
+#     device = session.get(Device, device_id)
+#     if not device:
+#         raise HTTPException(status_code=404, detail="Device not found")
+#     return device
 
 # Come back later and validate device has no repairs tied to it before deleting
 @router.delete("/{device_id}", response_model=DevicePublic)
-def delete_device(*, session: session_dep, device_id: uuid.UUID):
-    pass
-    # db_device = session.get(DeviceBase, device_id)
-    # if not db_device:
-    #     raise HTTPException(status_code=404, detail="Device not found")
-    # session.delete(db_device)
-    # session.commit()
-    # return db_device
+def delete_device(*, request: Request, session: session_dep, device_id: uuid.UUID):
+    associated_repairs = session.exec(
+        select(Repair).where(Repair.device_model_id == device_id)
+    ).all()
+
+    if associated_repairs:
+        return templates.TemplateResponse(
+            "components/notification.html",
+            {"request": request, "message": "Cannot delete a device model with associated repairs!", "type": "error"}
+        )
+    
+    db_device = session.get(Device, device_id)
+    if not db_device:
+        return templates.TemplateResponse(
+            name="components/notification.html",
+            context={"request": request, "message": "Cannot delete a device that does not exist!", "type": "error"}
+        )
+    
+    session.delete(db_device)
+    session.commit()
+    return templates.TemplateResponse(
+        name="components/notification.html",
+        context={"request": request, "message": "Device deleted successfully!", "type": "success"},
+        headers={"HX-Trigger": "refreshOverview"}
+    )
